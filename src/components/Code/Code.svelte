c0f0aa9484277440fc76ef8886e13f88d39e17af
rootEnginear <rootenginear@users.noreply.github.com>
2025-04-14 05:33:37 +0700

    fix: prepare github page deploy

--- src/components/Code/Code.svelte
+++ src/components/Code/Code.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
-	import { highlighter } from './instance.js';
+	import { highlightCode } from './instance.js';
 
 	export let code: string;
 	export let lang = 'svelte';
-
-	$: highlightedCode = highlighter.codeToHtml(code, {
-		lang,
-		theme: 'github-dark-default'
-	});
 </script>
 
 <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-{@html highlightedCode}
+{@html highlightCode(code, lang)}
--- src/components/Code/instance.ts
+++ src/components/Code/instance.ts
@@ -1,12 +1,15 @@
-import { createHighlighterCore } from 'shiki/core';
-import { createJavaScriptRegexEngine } from 'shiki/engine/javascript';
+import bash from '@shikijs/langs-precompiled/bash';
+import svelte from '@shikijs/langs-precompiled/svelte';
+import typescript from '@shikijs/langs-precompiled/typescript';
+import github_dark_default from '@shikijs/themes/github-dark-default';
+import { createHighlighterCoreSync } from 'shiki/core';
+import { createJavaScriptRawEngine } from 'shiki/engine/javascript';
 
-export const highlighter = await createHighlighterCore({
-	themes: [import('shiki/themes/github-dark-default.mjs')],
-	langs: [
-		import('shiki/langs/svelte.mjs'),
-		import('shiki/langs/typescript.mjs'),
-		import('shiki/langs/bash.mjs')
-	],
-	engine: createJavaScriptRegexEngine()
+export const highlighter = createHighlighterCoreSync({
+	themes: [github_dark_default],
+	langs: [svelte, typescript, bash],
+	engine: createJavaScriptRawEngine()
 });
+
+export const highlightCode = (code: string, lang = 'svelte') =>
+	highlighter.codeToHtml(code, { lang, theme: 'github-dark-default' });
