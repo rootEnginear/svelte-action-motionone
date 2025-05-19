037ed99a733e7005798e23843d7b8600872a0e24
rootEnginear <rootenginear@users.noreply.github.com>
2025-05-19 12:40:52 +0700

    fix(doc): shiki engine

--- src/components/Code/instance.ts
+++ src/components/Code/instance.ts
@@ -1,14 +1,16 @@
-import bash from '@shikijs/langs-precompiled/bash';
-import svelte from '@shikijs/langs-precompiled/svelte';
-import typescript from '@shikijs/langs-precompiled/typescript';
+import bash from '@shikijs/langs/bash';
+import svelte from '@shikijs/langs/svelte';
+import typescript from '@shikijs/langs/typescript';
 import github_dark_default from '@shikijs/themes/github-dark-default';
 import { createHighlighterCoreSync } from 'shiki/core';
-import { createJavaScriptRawEngine } from 'shiki/engine/javascript';
+import { createJavaScriptRegexEngine } from 'shiki/engine/javascript';
 
 export const highlighter = createHighlighterCoreSync({
 	themes: [github_dark_default],
 	langs: [svelte, typescript, bash],
-	engine: createJavaScriptRawEngine()
+	engine: createJavaScriptRegexEngine({
+		target: 'ES2018'
+	})
 });
 
 export const highlightCode = (code: string, lang = 'svelte') =>
