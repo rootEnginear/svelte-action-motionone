4d854c784326d7bf9e4d5ddae00ba952cbc3622b
rootEnginear <rootenginear@users.noreply.github.com>
2025-10-25 03:28:53 +0700

    fix(docs): document exported items

--- src/lib/actions/hover.ts
+++ src/lib/actions/hover.ts
@@ -5,6 +5,14 @@ type HoverParams =
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	Parameters<typeof motionHover> extends [infer _, ...infer Params] ? Params : never;
 
+/**
+ * Type for typing the parameters of the `hover` action.
+ *
+ * [👍 Read Best Practices](https://rootenginear.github.io/svelte-action-motionone/#best-practices)
+ * @example
+ * const hoverParams = [onHoverStart, options] satisfies HoverActionParams
+ * // <div use:hover={hoverParams} />
+ */
 export type HoverActionParams = HoverParams | ((node: HTMLElement) => HoverParams);
 
 const createHover = (node: HTMLElement) => (params: HoverActionParams) => {
@@ -12,6 +20,15 @@ const createHover = (node: HTMLElement) => (params: HoverActionParams) => {
 	return motionHover(node, onHoverStart, options);
 };
 
+/**
+ * Motion's `hover(elementOrSelector, onHoverStart, options)` equivalent.
+ * - [📑 Read Documentation](https://rootenginear.github.io/svelte-action-motionone/#use-hover)
+ * - [👍 Read Best Practices](https://rootenginear.github.io/svelte-action-motionone/#best-practices)
+ * @example
+ * <div use:hover={[onHoverStart, options]} />
+ * @example
+ * <div use:hover={(node) => [onHoverStart, options]} />
+ */
 export const hover: Action<HTMLElement, HoverActionParams> = (node, params) => {
 	const instanceHover = createHover(node);
 	let stop = instanceHover(params);
